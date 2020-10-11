68fbd2ad9161a88a7e611fb84ef0de8dc58c58de
Raúl Garrido <me@happybydefault.com>
2020-10-10 23:19:21 -0300

    early development

--- session.go
+++ session.go
@@ -104,7 +104,7 @@ func (s *session) handlePacket(ctx context.Context, pkt string) error {
 		return errEndOfService
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	switch id {
