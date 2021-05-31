540a09ec8cd1c8612604abf5f77b44331d512d3b
Raúl Garrido <me@happybydefault.com>
2021-05-31 16:53:37 -0400

    Revert "Fix context bug on session packet handling"
    This reverts commit 8aa90f89

--- session.go
+++ session.go
@@ -108,7 +108,7 @@ func (s *session) handlePacket(ctx context.Context, pkt string) error {
 		return errInvalidRequest
 	}
 
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	switch id {
